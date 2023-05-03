fe7c6c3d1c8c01beb8a2c6ca32e866c5aad94d89
slim56 <127349710+slim56@users.noreply.github.com>
2023-05-03 00:37:36 -0500

    Update Hx711.py

--- Hx711.py
+++ Hx711.py
@@ -1,50 +1,23 @@
-import RPi.GPIO as GPIO
 from hx711 import HX711
-from time import sleep
 
-#hx = HX711(dout_pin=17, pd_sck_pin=16)
-#hx.set_reading_format("MSB", "MSB")
-#hx.set_reference_unit(92)
-#hx.reset()
-#hx.tare()
+# initialize the HX711 module with the input and output pin numbers
+hx = HX711(dout_pin=17, pd_sck_pin=16)
 
-#while True:
-    #val = hx.get_weight()
-    #print(val)
-try:
-    hx711 = HX711(
-        dout_pin = 17,
-        pd_sck_pin = 16,
-        channel = 'B',
-        gain = 32
-        )
+# set the scale factor for the load cell
+hx.set_scale(1000)  # 1000 is the scale factor for my load cell
 
-    hx711.reset()
-    measures = hx711.get_raw_data(times=5)
-    '''
-    for i in range (5):
-        if measures[i]<measures[0]:
-            while measures[i]<measures[0]:
-                measures[i] = measures[i]+ 1
-                 '''
-    kw = int(input("Enter known weight: "))
-    calb_factor = []
-    for measure in measures:
-        calb_factor.append(((measure/kw)))
+# tare the load cell to zero
+hx.tare()
 
-    calb_factor = ((sum(calb_factor))/(len(calb_factor)))
-    print((int(calb_factor)))
-    
-    while True:
-        fir = (( (sum((hx711.get_raw_data(times= 5)))/5))/calb_factor)
-        #sec = (( (sum(hx711.get_raw_data(times= 20)))/20)/calb_factor)
-        print(((fir)))#+sec)/2))
-        hx711.reset()
-        
-        print(hx711.get_raw_data(times=2))
-        sleep(2)
+# get the weight from the load cell
+weight = hx.get_weight()
 
-finally:
-    GPIO.cleanup()
+print("Weight: {} grams".format(weight))
 
+# set a reference unit if you want to weigh in a specific unit (e.g., grams, ounces)
+hx.set_reference_unit(0.1)  # 0.1 gram per unit
 
+# get the weight in ounces
+weight_oz = hx.get_weight(ounces=True)
+
+print("Weight: {} ounces".format(weight_oz))
